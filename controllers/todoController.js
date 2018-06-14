0fd230c0846cf04965c7dc98e41bbd20249a0643
furkaninalcik <m.furkaninalcik@gmail.com>
2018-06-14 23:57:50 +0000

    initialized todo app

---
+++ controllers/todoController.js
@@ -0,0 +1,10 @@
+module.export = function(app){
+
+app.get('/todo', function(req,res){
+
+
+
+});
+
+
+};
---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+
+var todoController = require('./controllers/todoController');
+
+
+var app = express();
+
+//set up template engine
+
+app.set('view engine','ejs');
+
+
+//static files
+app.use(express.static('./public'));
+
+
+todoController(app);
+
+//listen port
+app.listen(8080);
+
+console.log('You are listening to port 8080!');
+
+
+
