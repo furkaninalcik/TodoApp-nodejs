a4e35191cd337918de1eb5a5d23b03f1e84c7743
furkaninalcik <m.furkaninalcik@gmail.com>
2018-06-15 08:11:43 +0300

    added src for todo-list.js

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,7 +1,9 @@
+var data = [{item: 'water'} , {item: 'milk'} , {item: 'tea'}];
+
 module.exports = function(app){
 
 	app.get('/todo', function(req,res){
-		res.render('todo');
+		res.render('todo' , {todos: data});
 
 
 	});
