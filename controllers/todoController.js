5127a73b98a5f5005a9c82a2a028aa40399d4fa6
furkaninalcik <m.furkaninalcik@gmail.com>
2018-06-15 10:41:55 +0300

    added the delete from the list functionality

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -22,7 +22,11 @@ module.exports = function(app){
 
 	app.delete('/todo/:item', function(req,res){
 
+		data = data.filter(function(todo){
+			return todo.item.replace(/ /g , '-') !== req.params.item;
+		});
 
+		res.json(data);
 		
 	});
 
