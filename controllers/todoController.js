1d9606435d5b785729e018faed073dc9127fffd8
furkaninalcik <m.furkaninalcik@gmail.com>
2018-06-15 03:36:24 +0300

    added todo.ejs

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,10 +1,22 @@
 module.export = function(app){
 
-app.get('/todo', function(req,res){
+	app.get('/todo', function(req,res){
+		res.render('todo');
+
+
+	});
+
+	app.post('/todo', function(req,res){
 
 
 
-});
+	});
+
+	app.delete('/todo', function(req,res){
+
+
+		
+	});
 
 
 };
